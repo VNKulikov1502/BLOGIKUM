831dffdde2df52c8b5ea894bc92d549ea4cf694d
VNKulikov1502 <90714112+VNKulikov1502@users.noreply.github.com>
2024-04-18 01:04:49 +0300

    Update settings.py
    alowwed hosts update

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'www.rodionloh.pythonanywhere.com',
-    'rodionloh.pythonanywhere.com',
+    'www.thirdgear.pythonanywhere.com',
+    'thirdgear.pythonanywhere.com',
     # Когда проект будет опубликован и станет доступен для пользователей,
     # в этот список нужно будет добавить и адреса домена, где он будет размещён
 ]
